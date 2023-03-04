da18ec2b911555030c1b6d6d68ee7972c7652cb1
Pedro López Murúa <pedro.lopez@prologic.cl>
2023-03-04 15:01:12 +0000

    card random

--- src/app.js
+++ src/app.js
@@ -1,11 +1,30 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+//crea las variables de la carta.
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+let valor = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+let pinta = ["♥", "♦", "♠", "♣"];
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+function creaValor() {
+  let randomValor = Math.floor(Math.random() * valor.length);
+  return valor[randomValor];
+}
+
+function creaPinta() {
+  let randomPinta = Math.floor(Math.random() * pinta.length);
+  return pinta[randomPinta];
+}
+
+function onLoad() {
+  let pinta = document.querySelector("#pinta");
+  let valor = document.querySelector("#valor");
+  let pinta2 = document.querySelector("#pinta2");
+  let x = creaPinta(pinta);
+  if (x == "♥" || x == "♦") {
+    pinta.className = "p-2 text-danger";
+    pinta2.className = "p-2 text-danger";
+    valor.className = "p-5 text-center text-danger";
+  }
+  pinta.innerHTML = x;
+  valor.innerHTML = creaValor(valor);
+  pinta2.innerHTML = x;
+}
+onLoad();
